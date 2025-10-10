b940f135a419f76ca04abca61d3ca0c65341e8e5
zakaria-amghare <zizou.amgh@gmail.com>
2025-10-10 23:16:11 +0100

    small improvment to kkep the spirite up

--- 00test.py
+++ 00test.py
@@ -3,4 +3,5 @@ import os
 import time
 for i in range(2):
     cprint("this file would duplicate theoraticly exponantialy if we alter it ","yellow")
-    os.system(f"copy test.py zaki{i}.py")
+    file=os.path.basename(__file__)
+    os.system(f"copy test.py {i}{file}")
---
+++ exscute.py
@@ -0,0 +1,4 @@
+import os
+os.system(f"python ./test.py")
+for i in range(2):
+    os.system(f"python ./{i}test.py")
