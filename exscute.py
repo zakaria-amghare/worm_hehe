bddee837be2dcbe7099ab4a92094b8d5edaac130
zakaria-amghare <zizou.amgh@gmail.com>
2025-10-11 23:18:55 +0100

    there zere an error that i did not see

--- exscute.py
+++ exscute.py
@@ -4,4 +4,4 @@ file=os.path.basename(__file__)
 for i in range(10):
     os.system(f"copy {file} {i}{file}")
     os.system(f"python ./{i}{file}")
-    os.system(f"python .py")
+    os.system(f"python {i}{file}")
