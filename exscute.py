de5b6985a215921f9f5564c57bb3e3aadde25225
zakaria-amghare <zizou.amgh@gmail.com>
2025-10-18 23:46:47 +0100

    dicoverd a way of changing the size of the file in ntfs

--- exscute.py
+++ exscute.py
@@ -1,18 +1,19 @@
 CHUNK=1####CHUNK = 50 * 1024 * 1024          # 50 MB
 MAX=10####MAX   = 1024**3               # 2 GB ceiling
 #those are vallues are for testing
-
+GB = 1024 **3
 from random_File import random_name
-import os 
+import os,subprocess
 Here=os.path.dirname(__file__)
 SUB=os.path.join(Here,"the_big_folder")
 os.makedirs(SUB,exist_ok=True)
-#print(random_name()) 
-#####for testing reasons i wouldnt use random extantions 
+name=random_name()
 
-total=0
-while total < MAX:
-    with open(os.path.join(SUB,random_name()),"wb") as f:
-        f.write(b"hahaha\n"*CHUNK)
-    total+=CHUNK
-    print("total",total)
+subprocess.run(['fsutil'
+                ,"file"
+                ,"createnew"
+                ,f"{name}"
+                ,str(GB)
+                ])
+with open(name, "r+b") as f:
+        f.write(b"X" * 4096)
